941e187a7de7001ad80761f3b1d63992e8f0ccfd
ketan <ketan.gupta@karexpert.com>
2020-11-24 23:33:50 +0530

    add spring clound hystrix dashboard

---
+++ src/main/java/com/ketan/SpringCloudServerHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.ketan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServerHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerHystrixDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ketan/SpringCloudServerHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ketan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServerHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
